479585d2614246e08f104c751ace4b2ae7cbf2f9
SOunit <workbookwork7@gmail.com>
2021-08-14 16:47:26 -0700

    add sample, arrow function

---
+++ samples/02_arrow_function/script.js
@@ -0,0 +1,27 @@
+console.log('hello from js');
+
+// arrow function is new way of creating function
+
+// function sample, old fashion
+function old_function() {
+  console.log('test');
+}
+
+// arrow function sample
+const new_arrow_function = () => {
+  console.log('test');
+};
+
+// both is same
+old_function();
+new_arrow_function();
+
+// simplest arrow function
+() => {};
+
+// simple number
+5;
+
+// same structure
+const number = 5;
+const arrowFunc = () => {};
