ef30041af56bc180f41ed2a9a22d0b2a905de953
SOunit <workbookwork7@gmail.com>
2021-08-17 16:11:38 -0700

    fix padding calc logic

--- samples/08_scroll_event/script.js
+++ samples/08_scroll_event/script.js
@@ -3,15 +3,20 @@ console.log('hello from js');
 const headerTag = document.querySelector('.header');
 const headerTagHeight = headerTag.clientHeight;
 
+const mainTag = document.querySelector('.main');
+const styles = window.getComputedStyle(mainTag);
+const { paddingTop } = styles;
+const paddingTopNum = parseInt(paddingTop.split('px')[0]);
+console.log(paddingTopNum);
+
+const rem = 16;
 window.addEventListener('scroll', () => {
   let scrollFromTop = window.pageYOffset;
 
-  const rem = 16;
-  const marginTop = 8 * rem;
   let scrollFromTopByRem = Math.floor(scrollFromTop / rem);
-  let styleChangePoint = scrollFromTop > marginTop - headerTagHeight;
+  let styleChangePoint = scrollFromTop > paddingTopNum - headerTagHeight;
   if (styleChangePoint) {
-    console.log('over 8 rem!!', scrollFromTopByRem);
+    console.log(scrollFromTopByRem);
     if (!headerTag.classList.contains('header--scrolled')) {
       headerTag.classList.add('header--scrolled');
     }
