9145537d301538ecffff7589b1697d7123eeba28
SOunit <workbookwork7@gmail.com>
2021-08-22 08:56:48 -0700

    add slide sample

---
+++ samples/09_slide_show/script.js
@@ -0,0 +1,18 @@
+console.log('hello from js');
+
+const target1 = document.querySelector('.js-slide-override-active-1');
+const target2 = document.querySelector('.js-slide-override-active-2');
+const target3 = document.querySelector('.js-slide-override-active-3');
+
+setTimeout(() => {
+  console.log('1000');
+  target1.classList.add('slide__override-top--active');
+}, 1000);
+setTimeout(() => {
+  console.log('2000');
+  target2.classList.add('slide__override-middle--active');
+}, 2000);
+setTimeout(() => {
+  console.log('3000');
+  target3.classList.add('slide__override-bottom--active');
+}, 3000);
