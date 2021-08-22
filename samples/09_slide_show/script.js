0e2ab649fe480f7e3623eeba8b8bea201f27abc7
SOunit <workbookwork7@gmail.com>
2021-08-22 09:00:57 -0700

    add wait seconds

--- samples/09_slide_show/script.js
+++ samples/09_slide_show/script.js
@@ -4,15 +4,17 @@ const target1 = document.querySelector('.js-slide-override-active-1');
 const target2 = document.querySelector('.js-slide-override-active-2');
 const target3 = document.querySelector('.js-slide-override-active-3');
 
+const wait = 0;
+
 setTimeout(() => {
   console.log('1000');
   target1.classList.add('slide__override-top--active');
-}, 1000);
+}, 1000 + wait * 1000);
 setTimeout(() => {
   console.log('2000');
   target2.classList.add('slide__override-middle--active');
-}, 2000);
+}, 2000 + wait * 1000);
 setTimeout(() => {
   console.log('3000');
   target3.classList.add('slide__override-bottom--active');
-}, 3000);
+}, 3000 + wait * 1000);
