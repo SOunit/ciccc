c3af4e68b61bb980c7ae30103f5984304067c859
SOunit <workbookwork7@gmail.com>
2021-09-16 16:59:16 -0700

    add sample

---
+++ 2021_09/16_js/02_sample/script.js
@@ -0,0 +1,14 @@
+const numbers = [1, 2, 3];
+
+// define a function
+// create a space for input
+const calc = (input) => {
+  console.log(input);
+  // 3 = 1, 2
+  const output = input[0] + input[1];
+  console.log(output);
+};
+
+// call
+// pass a value as a input
+calc(numbers);
