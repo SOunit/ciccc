fbcc89fe59fb67cd30d853931ac3e630a8fc52c2
SOunit <workbookwork7@gmail.com>
2021-09-23 16:39:36 -0700

    refactor, add comment

--- 2021_09/23_js_algo/03_assignment/03_isSorted/script.js
+++ 2021_09/23_js_algo/03_assignment/03_isSorted/script.js
@@ -30,31 +30,33 @@
 // }
 
 {
-  function isDesc(arr) {
-    return arr.every(function (left, i) {
+  const isDesc = (arr) => {
+    return arr.every((left, i) => {
       const right = arr[i - 1];
       return i === 0 || left >= right;
     });
-  }
+  };
 
-  function isAsc(arr) {
-    return arr.every(function (left, i) {
+  const isAsc = (arr) => {
+    return arr.every((left, i) => {
       const right = arr[i - 1];
       return i === 0 || left <= right;
     });
-  }
+  };
 
-  function isSorted(arr) {
+  const isSorted = (arr) => {
     console.log(arr);
     return isDesc(arr) || isAsc(arr);
-  }
+  };
 
+  // true
   console.log(isSorted([]));
   console.log(isSorted([42]));
   console.log(isSorted([39, 42]));
-  console.log(isSorted([42, 39]));
   console.log(isSorted([1, 2, 3, 4, 5]));
+  console.log(isSorted([42, 39]));
   console.log(isSorted([5, 4, 3, 2, 1]));
+  // false
   console.log(isSorted([1, 5, 2]));
   console.log(isSorted([1, 2, 3, 6, 8, 5, 1, 6]));
 }
