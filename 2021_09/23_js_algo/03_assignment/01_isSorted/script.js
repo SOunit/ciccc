da6fa0ef7d686cf35d85026d9b412c2fd2e9af88
SOunit <workbookwork7@gmail.com>
2021-09-23 12:44:36 -0700

    add sample

--- 2021_09/23_js_algo/03_assignment/01_isSorted/script.js
+++ 2021_09/23_js_algo/03_assignment/01_isSorted/script.js
@@ -28,6 +28,11 @@
     console.log('isDesc(arr)', isDesc(arr));
     console.log('isAsc(arr)', isAsc(arr));
 
+    // return true / false
+    // 1. desc=true, asc=true => true
+    // 2. desc=true, asc=false => true
+    // 3. desc=false, asc=true => true
+    // 4. desc=false, asc=false => false
     return isDesc(arr) || isAsc(arr);
   };
 
@@ -37,4 +42,5 @@
   console.log(isSorted([1, 2, 3]));
   console.log(isSorted([3, 2, 1]));
   console.log(isSorted([3, 2, 4, 1]));
+  console.log(isSorted([1, 4, 7, 3, 2]));
 }
