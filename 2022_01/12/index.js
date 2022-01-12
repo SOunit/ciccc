ae7470427606a4a9b517a81c9f9944857de9dec3
SOunit <workbookwork7@gmail.com>
2022-01-12 10:43:43 -0800

    add sample

---
+++ 2022_01/12/index.js
@@ -0,0 +1,42 @@
+/* xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+//Dummy Data
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx*/
+let gunter = {
+  name: "Gunter",
+  origin: "Adventure Time",
+  canFly: false,
+  sayHello: function () {
+    console.log("QUACK!!!");
+  },
+};
+
+let ramon = {
+  name: "Ramón",
+  origin: "Happy Feet",
+  canFly: true,
+  sayHello: function () {
+    console.log("Estoy encantado de conocerle.");
+  },
+};
+
+let fred = {
+  name: "Fred",
+  origin: "Sitting Ducks",
+  canFly: false,
+  sayHello: function () {
+    console.log("Hi there!");
+  },
+};
+
+//For question 5:
+let myPenguin = {
+  character: "Captain Cook",
+  origin: "Mr. Popper's Penguins",
+  author: "Richard and Florence Atwater",
+  notes:
+    "Children's book about a housepainter who ends up with a flock of penguins.",
+};
+
+/* xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+  //End of Dummy Data
+  xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx*/
