45918223a05d2cbe8f9dfeaef5e6f320f080108e
SOunit <workbookwork7@gmail.com>
2022-05-17 13:24:42 -0700

    add answer

--- 2022-05/17-mongodb/src/routes/index.js
+++ 2022-05/17-mongodb/src/routes/index.js
@@ -1,43 +1,43 @@
-const uuid = require("uuid");
-const router = require("express").Router();
-const { client } = require("../services/redis");
+const uuid = require('uuid')
+const router = require('express').Router()
+const { client } = require('../services/redis')
 
-router.get("/", async (req, res) => {
+router.get('/', async (req, res) => {
   try {
-    const todos = [];
+    const todos = []
 
-    for await (const { field, value } of client.hScanIterator("todos")) {
-      todos.push({ ID: field, Title: value });
+    for await (const { field, value } of client.hScanIterator('todos')) {
+      todos.push({ ID: field, Title: value })
     }
 
-    res.render("index", { model: todos });
+    res.render('index', { model: todos })
   } catch (e) {
-    console.error(e);
+    console.error(e)
   }
-});
+})
 
-router.get("/create", (req, res) => {
-  res.render("create", { model: {} });
-});
+router.get('/create', (req, res) => {
+  res.render('create', { model: {} })
+})
 
-router.post("/create", async (req, res) => {
-  await client.hSet("todos", uuid.v4(), req.body.Title);
-  res.redirect("/");
-});
+router.post('/create', async (req, res) => {
+  await client.hSet('todos', uuid.v4(), req.body.Title)
+  res.redirect('/')
+})
 
-router.get("/edit/:id", async (req, res) => {
-  const todo = await client.hGet("todos", ID);
-  res.render("edit", { model: { ID: req.params.id, Title: todo } });
-});
+router.get('/edit/:id', async (req, res) => {
+  const todo = await client.hGet('todos', ID)
+  res.render('edit', { model: { ID: req.params.id, Title: todo } })
+})
 
-router.post("/edit/:id", async (req, res) => {
-  await client.hSet("todos", req.params.id, req.body.Title);
-  res.redirect("/");
-});
+router.post('/edit/:id', async (req, res) => {
+  await client.hSet('todos', req.params.id, req.body.Title)
+  res.redirect('/')
+})
 
-router.delete("/delete/:id", async (req, res) => {
-  await client.hDel("todos", req.params.id);
-  res.redirect("/");
-});
+router.delete('/delete/:id', async (req, res) => {
+  await client.hDel('todos', req.params.id)
+  res.redirect('/')
+})
 
-module.exports = router;
+module.exports = router
--- 2022-05/17-mongodb/src/services/mongo.js
+++ 2022-05/17-mongodb/src/services/mongo.js
@@ -2,16 +2,16 @@ const MongoClient = require('mongodb').MongoClient
 require('dotenv').config()
 
 let dbo,
-    dbConnectionStr = process.env.MONGO_URL,
-    dbName = 'm10921todos'
+  dbConnectionStr = process.env.MONGO_URL,
+  dbName = 'm10921todos'
 
 exports.mongoConnect = () => {
-    MongoClient.connect(dbConnectionStr)
-    .then(client => {
-        console.log('Connected to Database!');
-        dbo = client.db(dbName)
+  MongoClient.connect(dbConnectionStr)
+    .then((client) => {
+      console.log('Connected to Database!')
+      dbo = client.db(dbName)
     })
-    .catch(err => console.error('Error in mongo connect: ', err))
+    .catch((err) => console.error('Error in mongo connect: ', err))
 
-    return dbo
+  return dbo
 }
